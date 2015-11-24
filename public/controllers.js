3721daadbe841edf55de74d99fbecf848359d70f
bgveenstra <b.g.veenstra@gmail.com>
2015-11-24 07:20:55 -0800

    rm answers from controllers.js

--- public/controllers.js
+++ public/controllers.js
@@ -12,46 +12,25 @@ angular.module('myApp.controllers', [])
   //POSTS
   .controller('PostsIndexCtrl', ['$scope', '$location', '$http', function ($scope, $location, $http) {
     // GET POSTS
-    $http.get('/api/posts')
-      .success(function (data) {
-        // this callback will be called asynchronously
-        // when the response is available
-        $scope.posts = data;
-      })
-      .error(function (data) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-        alert("error: ", data);
-      });
+    // make a GET request for all posts
 
     // NEW POST
-    $scope.post = {};
+    // create an empty 'post' object within the scope
 
 
     // CREATE A POST    
     $scope.createPost = function() {
-      $http.post('/api/posts', $scope.post)
-        .success(function(data){
-          $scope.posts.unshift(data);
-        })
-        .error(function(data) {
-          alert("there was a problem saving your post");
-        });
-      // reset post object
-      $scope.post = {};
+      // make a POST request to create the post based on scope's post object
+
+      // reset scope's post object
+      
     };
 
 
     // DELETE A POST
     $scope.deletePost = function(post) {
-      $http.delete('/api/posts/' + post._id)
-          .success(function(data){
-            var index = $scope.posts.indexOf(post)
-            $scope.posts.splice(index, 1);          
-          })
-          .error(function(data) {
+      // make a DELETE request for this post
 
-          });
     };
 
 
